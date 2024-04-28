4d7022eb61ac728797a6d74d13e9a0ede948039f
Muhammad Hassan Saeed <121416861+M-HassanSaeed@users.noreply.github.com>
2024-04-28 17:20:19 +0500

    Update scrape_file.js

--- src/scrape_file.js
+++ src/scrape_file.js
@@ -1,7 +1,10 @@
 const { chromium } = require('playwright');
 
 async function main(noticeNumber, taxPayerID) {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.connect({
+    wsEndpoint: 'wss://yourrr-railway-app.railway.app/playwright?token=YOUR_TOKEN',
+  });
+
   const context = await browser.newContext();
   const page = await context.newPage();
 
@@ -15,16 +18,23 @@ async function main(noticeNumber, taxPayerID) {
   await page.fill("//input[contains(@name, 'Dc-a')]", taxPayerID);
   await page.click("//button[contains(@data-name, 'Dc-c')]");
 
-  await page.waitForTimeout(2000);
-
+  await page.waitForTimeout(1000);
   const nonCompliance = await page.textContent('#caption2_Dc-k');
+  await page.waitForTimeout(1000);
   const certificateNotFound = await page.textContent('#caption2_Dc-g');
+  await page.waitForTimeout(1000);
   const issueDate = await page.textContent('#caption2_Dc-h');
+  await page.waitForTimeout(1000);
   const date = await page.getAttribute('#Dc-h', 'value');
+  await page.waitForTimeout(1000);
   const issuedTo = await page.textContent('#caption2_Dc-i');
+  await page.waitForTimeout(1000);
   const issuedToName = await page.getAttribute('#Dc-i', 'value');
+  await page.waitForTimeout(1000);
   const caption2 = await page.textContent('#caption2_Dc-j');
+  await page.waitForTimeout(1000);
   const caption2Element = await page.locator('//span[@id="caption2_Dc-l" and contains(@class, "CTE") and contains(@class, "CaptionLinkText") and text()="Click here to request a current Certificate of Clean Hands for this taxpayer."]');
+  await page.waitForTimeout(1000);
   const caption2Visible = await caption2Element.isVisible();
   if (!date) {
     console.log(certificateNotFound);
@@ -33,6 +43,7 @@ async function main(noticeNumber, taxPayerID) {
   } else {
     console.log(issueDate, date, issuedTo, issuedToName, caption2);
   }  
+
   await browser.close();
 }
 
